b88f1ea7de2c18dc038888cdec2caca09525a5dc
pavan <pavankalvakota@gmail.com>
2024-03-13 18:13:31 +0530

    Created the Posts model, configured the setting for handling the static media files like photos, and modified the blogs.urls accordingly

--- artifex/blog/models.py
+++ artifex/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date_posted = models.DateTimeField(default =timezone.now)
+    image = models.ImageField(upload_to='artwork_images/')
+
+
+    def __str__(self):
+        return self.title
--- artifex/blog/urls.py
+++ artifex/blog/urls.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name="blog-home")
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
